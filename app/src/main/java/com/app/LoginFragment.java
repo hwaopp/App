2b9109c8f02717ec5d7d0783a8c0bbbcea439c0f
henry8131joy <henry8131joy@gmail.com>
2022-01-12 23:38:22 +0800

    第3次 新增記住帳號密碼

--- app/src/main/java/com/app/LoginFragment.java
+++ app/src/main/java/com/app/LoginFragment.java
@@ -1,6 +1,8 @@
 package com.app;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,6 +17,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 import com.app.member.MemberBean;
@@ -28,6 +31,8 @@ public class LoginFragment extends Fragment {
     private Activity activity;
     private Button button;
     private TextInputEditText edUsername, edPassword;
+    private CheckBox checkBox;
+    private SharedPreferences sharedPreferences;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,7 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         activity = getActivity();
+        sharedPreferences = activity.getSharedPreferences("user", Context.MODE_PRIVATE);
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
@@ -45,13 +51,27 @@ public class LoginFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         findViews(view);
+        handleEditTest();
+        handleCheckBox();
         login();
+    }
+
+    private void handleCheckBox() {
+        checkBox.setChecked(sharedPreferences.getBoolean("remember", false));
+    }
+
+    private void handleEditTest() {
+       final String username = sharedPreferences.getString("username", "");
+       final String password =  sharedPreferences.getString("password", "");
+       edUsername.setText(username);
+       edPassword.setText(password);
     }
 
     private void findViews(View view) {
         edUsername = view.findViewById(R.id.edUsername);
         edPassword = view.findViewById(R.id.edPassword);
         button = view.findViewById(R.id.button);
+        checkBox = view.findViewById(R.id.checkBox);
     }
 
     private void login() {
@@ -88,6 +108,19 @@ public class LoginFragment extends Fragment {
                 if ("帳號或密碼錯誤".equals(respMemberBean.getMessage())){
                     Toast.makeText(activity, respMemberBean.getMessage(), Toast.LENGTH_SHORT).show();
                 }else{
+                    if (checkBox.isChecked()){
+                        sharedPreferences.edit()
+                                .putString("username", String.valueOf(username))
+                                .putString("password", String.valueOf(password))
+                                .putBoolean("remember", checkBox.isChecked())
+                                .apply();
+                    } else {
+                        sharedPreferences.edit()
+                                .remove("username")
+                                .remove("password")
+                                .remove("remember")
+                                .apply();
+                    }
                     NavController navController = Navigation.findNavController(view);
                     navController.navigate(R.id.action_loginFragment_to_resultFragment);
                     Toast.makeText(activity, respMemberBean.getNickname() + "\n登入成功", Toast.LENGTH_SHORT).show();
